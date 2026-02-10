29ff6cf934a4af7c35673226f8ceae17d0be298e
Lalila-Diya <diyaj.696969@gmail.com>
2026-02-10 22:48:38 +0530

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,13 @@
+#ACP- Tuple product
+def productfun(num):
+    List1=list(num)
+    mult = 1
+    for a in list:
+        mult=mult * a 
+        return mult
+    
+
+n = (2,3,-5)
+print ("Tuple: ",n)
+
+print("Product are: ",productfun (n) )
